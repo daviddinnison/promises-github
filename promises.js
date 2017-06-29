0ee084cbb7c6cbe283c755817e08f28c91452ed6
daviddinnison <david.g.dinnison@gmail.com>
2017-06-29 14:50:40 -0400

    promises.js implemented

--- promises.js
+++ promises.js
@@ -1,12 +1,22 @@
 // Parallel Promises
-
-/* Test URLS
-https://api.github.com/users/dankato
-https://api.github.com/users/daviddinnison
-https://api.github.com/users/cklanac
-https://api.github.com/users/mscottcam
-https://api.github.com/users/lewi-g
-*/
+let dankato = fetch('https://api.github.com/users/dankato');
+let daviddinnison = fetch('https://api.github.com/users/daviddinnison');
+let cklanac = fetch('https://api.github.com/users/cklanac');
+let mscottcam = fetch('https://api.github.com/users/mscottcam');
+let lewig = fetch('https://api.github.com/users/lewi-g');
 
 
-
+Promise.all([dankato, daviddinnison, cklanac, mscottcam, lewig])
+	.then(responses => {
+		let results = responses.map( stream => stream.json() );
+		console.log(results);
+		return Promise.all(results); 
+	}).then( results => {
+		results.forEach(function(results) {
+			//public repos
+			console.log(results.name + " public repos: ", results.public_repos);
+			return results.public_repos;
+		});
+	}).catch(error => {
+    	console.log(error);
+});
