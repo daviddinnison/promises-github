78ff0771ff3b9912c110b3a8d17a040f066e0a4b
dankato <apearmor@gmail.com>
2017-06-29 10:19:52 -0700

    2nd lunch commit, added 2ndary files

--- github.js
+++ github.js
@@ -4,8 +4,29 @@ $.getJSON('https://api.github.com/users/Thinkful/repos', data => {
 
 // GitHub doesn't need to respond before your code advances to here; it's asynchronous
 
-
-
 fetch('https://api.github.com/users/Thinkful/repos').then(response => {
     // Waits until you have the response from GitHub then this code is run
 });
+
+/* Test URLS
+https://api.github.com/users/dankato
+https://api.github.com/users/daviddinnison
+https://api.github.com/users/cklanac
+https://api.github.com/users/mscottcam
+https://api.github.com/users/lewi-g
+*/
+
+// Single Fetch Call with .then()
+fetch('https://api.github.com/users/dankato').then(() => {
+    console.log('First request has completed');
+    return fetch('http://example.com/second-endpoint');
+})
+.then(() => {
+    console.log('Second request has completed');
+});
+
+
+
+// Add a .catch() to the promise
+
+// 
---
+++ promises.js
@@ -0,0 +1,12 @@
+// Parallel Promises
+
+/* Test URLS
+https://api.github.com/users/dankato
+https://api.github.com/users/daviddinnison
+https://api.github.com/users/cklanac
+https://api.github.com/users/mscottcam
+https://api.github.com/users/lewi-g
+*/
+
+
+
